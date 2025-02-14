f4eb1f37de98a6f156c98fae1600ce35c976e7ae
Dayton Jones <jones.dayton@gmail.com>
2025-02-14 21:08:41 +0000

    background, rename uploads

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -11,8 +11,8 @@ def create_app():
     base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(base_dir, 'data', 'rca.db')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    # Directory for custom files (logos, images, etc.) in persistent "uploads"
-    app.config['UPLOAD_FOLDER'] = os.path.join(base_dir, 'uploads')
+    # Directory for custom files (logos, images, etc.) in persistent "custom_assets"
+    app.config['UPLOAD_FOLDER'] = os.path.join(base_dir, 'custom_assets')
     
     db.init_app(app)
     
@@ -20,5 +20,10 @@ def create_app():
         from app import routes
         db.create_all()
     
+    # Route to serve custom assets (e.g. background image)
+    @app.route('/custom_assets/<path:filename>')
+    def custom_assets(filename):
+        return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
     return app
 
